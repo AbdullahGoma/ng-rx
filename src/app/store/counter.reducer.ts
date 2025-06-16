c988b78265db402051b9ede9590a8d11c47b1c0e
AbdullahGomaa <Abdullah.Goma.010170@gmail.com>
2025-06-16 22:13:45 +0300

    Another way to create reducer

--- src/app/store/counter.reducer.ts
+++ src/app/store/counter.reducer.ts
@@ -2,6 +2,10 @@ import { createReducer } from "@ngrx/store";
 
 const initialState = 0; // It can be anything (object, array, ...)
 
-export const counterReducer = createReducer(
-    initialState
-);
+// export const counterReducer = createReducer(
+//     initialState
+// );
+
+export function counterReducer(state = initialState) {
+    return state;
+} 
