1a783515646484c78c022c7cf7b286ff0d7fd172
AbdullahGomaa <Abdullah.Goma.010170@gmail.com>
2025-06-17 18:35:37 +0300

    handle the action with function

--- src/app/store/counter.reducer.ts
+++ src/app/store/counter.reducer.ts
@@ -3,11 +3,11 @@ import { increment } from './counter.actions';
 
 const initialState = 0; // It can be anything (object, array, ...)
 
-export const counterReducer = createReducer(
-  initialState,
-  on(increment, (state, action) => state + action.value)
-);
+// export const counterReducer = createReducer(
+//   initialState,
+//   on(increment, (state, action) => state + action.value)
+// );
 
-// export function counterReducer(state = initialState) {
-//     return state;
-// }
+export function counterReducer(state = initialState, action: any) {
+  return action.type === '[Counter] Increment' ? state + action.value : state;
+}
