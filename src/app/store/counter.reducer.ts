9320ab448dbc9f0ac0bc5956b3ec87dc77bb4c75
AbdullahGomaa <Abdullah.Goma.010170@gmail.com>
2025-06-17 18:21:30 +0300

    dispatch the action

--- src/app/store/counter.reducer.ts
+++ src/app/store/counter.reducer.ts
@@ -5,7 +5,7 @@ const initialState = 0; // It can be anything (object, array, ...)
 
 export const counterReducer = createReducer(
     initialState,
-    on(increment, (state) => state++)
+    on(increment, (state) => state + 1)
 );
 
 // export function counterReducer(state = initialState) {
